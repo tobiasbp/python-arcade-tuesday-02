75637312e10c2e291f1d087e31a17932534500a4
Ashthecool <106169851+Ashthecool@users.noreply.github.com>
2023-04-04 17:29:24 +0200

    Found out to make different colors. Right now it's ORANGE, FIRE_ENGINE_RED, RED_ORANGE

--- tools.py
+++ tools.py
@@ -7,20 +7,12 @@ import arcade
 from typing import Tuple
 import random
 
-def wrap(sprite: arcade.Sprite, max_x: int, max_y: int):
-    """
-    if sprite is off-screen move it to the other side of the screen
-    """
+PARTICLE_COLORS = [
+    arcade.color.ORANGE,
+    arcade.color.FIRE_ENGINE_RED,
+    arcade.color.RED_ORANGE
+]
 
-    if sprite.right < 0:
-        sprite.center_x += max_x
-    elif sprite.left > max_x:
-        sprite.center_x -= max_x
-
-    if sprite.top < 0:
-        sprite.center_y += max_y
-    elif sprite.bottom > max_y:
-        sprite.center_y -= max_y
 
 def get_joystick(func_press, func_release=None, func_axis=None, func_jhat=None):
     """
@@ -66,14 +58,15 @@ class StoppableEmitter:
                  target: arcade.Sprite,
                  particle_lifetime: float = 0.5,
                  noise: int = 15,
-                 offset: Tuple[int] = (0, 6),
+                 offset: Tuple[int] = (0, 5),
                  emit_interval: float = 0.01,
                  particle_count: int = 30,
-                 start_alpa: int = 100):
+                 start_alpha: int = 100):
         self.target = target
         self.noise = noise
         self.emit_interval = emit_interval
         self.particle_count = particle_count
+        self.particle_color = PARTICLE_COLORS[random.randint(0, 2)]
 
         # Emit controller enters endless loop with an interval of 0
         assert self.emit_interval > 0, "Emit interval must be greater than 0"
@@ -83,10 +76,10 @@ class StoppableEmitter:
             center_xy=target.position,
             emit_controller=arcade.EmitterIntervalWithCount(self.emit_interval, 0),
             particle_factory=lambda emitter: arcade.FadeParticle(
-                filename_or_texture=arcade.make_circle_texture(random.randint(7, 30), arcade.color.ORANGE),
+                filename_or_texture=arcade.make_circle_texture(random.randint(7, 30), self.particle_color),
                 change_xy=offset,
                 lifetime=particle_lifetime,
-                start_alpha=start_alpa
+                start_alpha=start_alpha
             )
         )
 
@@ -106,3 +99,4 @@ class StoppableEmitter:
         self.emitter.center_x, self.emitter.center_y = self.target.position
         self.emitter.angle = (self.target.angle + 90) + random.randint(-1 * self.noise, self.noise)
         self.emitter.update()
+        self.particle_color = PARTICLE_COLORS[random.randint(0, 2)]
